672fa08c1eed71760abc133b36332a7e11231867
AlexNullDeveloper <talismanoff1990@yandex.ru>
2016-07-30 19:27:19 +0300

    parser changes and correcting CatalogofEmailsDB

--- src/main/java/ru/smarttara/catalogs/CatalogOfEmailsDB.java
+++ src/main/java/ru/smarttara/catalogs/CatalogOfEmailsDB.java
@@ -1,8 +1,5 @@
 package ru.smarttara.catalogs;
 
-/**
- * Created by ZXCASD on 17.07.2016.
- */
 
 import ru.smarttara.util.JdbcHelper;
 
@@ -15,7 +12,7 @@ import java.sql.SQLException;
  * class that help to work with catalog of emails
  * Created by a.talismanov on 01.06.2016.
  */
-public class CatalogOfEmailsDB {
+class CatalogOfEmailsDB {
 
     /**
      * Connection to database
@@ -26,10 +23,10 @@ public class CatalogOfEmailsDB {
      * Retuirns resultSet with data from table EMAILS
      * @return ResultSet from table Emails
      */
-    public static ResultSet getResultSetFromTableEmails() {
+    static ResultSet getResultSetFromTableEmails() {
         connect = JdbcHelper.getConnection();
         ResultSet rs = null;
-        String selectDataFromEmails = "SELECT E_MAIL, IS_SENDED, COMPANY_NAME, PHONE_NUMBER, CATEGORY_ID, ADDING_DATE " +
+        String selectDataFromEmails = "SELECT E_MAIL, IS_SENDED, COMPANY_NAME, PHONE_NUMBER, CATEGORY, ADDING_DATE " +
                 "FROM EMAILS";
 
 
--- src/main/java/ru/smarttara/csvparsers/CsvEmailParser.java
+++ src/main/java/ru/smarttara/csvparsers/CsvEmailParser.java
@@ -5,7 +5,10 @@ import ru.smarttara.util.JdbcHelper;
 import java.io.*;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.HashSet;
+import java.util.Set;
 
 public class CsvEmailParser {
 
@@ -17,18 +20,46 @@ public class CsvEmailParser {
     }
 
     private static void parse(String filePath) {
-        System.setProperty("file.encoding", "UTF-8");
         //zamenit' potom
-//        filePath = "D:\\email_import.csv";
+//        filePath = "D:\\email_import_2.csv";
         Connection connection = JdbcHelper.getConnection();
+        Set<String> uniqueMails = new HashSet<>();
+
+        String sqlSelectMails = "SELECT e_mail FROM emails";
 //        PreparedStatement preparedStatement  = null;
         String sql = "INSERT INTO EMAILS(E_MAIL, IS_SENDED, COMPANY_NAME, PHONE_NUMBER, CATEGORY, ADDING_DATE, ADRESS, SITE) VALUES(?,?,?,?,?,now(),?,?)";
         String line = "";
         String csvSplitBy = ";";
-        try (BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(new FileInputStream(filePath), "UTF8"));
+        System.out.println("filepath = " + filePath);
+
+
+        try (BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(new FileInputStream(filePath), "Cp1251"));
+//             PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
+             PreparedStatement preparedStatementSelect = connection.prepareStatement(sqlSelectMails);
              PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
+
+            ResultSet resultSet = preparedStatementSelect.executeQuery();
+            while (resultSet.next()) {
+                uniqueMails.add(resultSet.getString(1));
+            }
+
+
             while ((line = bufferedReader.readLine()) != null) {
                 String[] emailData = line.split(csvSplitBy);
+
+
+                //trimming phone
+                emailData[2] = emailData[2].trim().replaceAll(" ","").replaceAll("-","");
+//                System.out.println("Phone " + emailData[2]);
+//                System.out.println("Phone.length " + emailData[2].length());
+
+                if (uniqueMails.contains(emailData[0])) {
+                    System.out.println("dublicate email..." + emailData[0] + " skipping this");
+                    continue;
+                }
+
+
+                System.out.println("data " + emailData[0] + " " + emailData[1] + " " + emailData[2] + " " + emailData[3] + " " + emailData[4] + " " + emailData[5]);
                 preparedStatement.setString(1, emailData[0]); //E_MAIL
                 preparedStatement.setInt(2, NOT_SENDED);//IS_SENDED
                 preparedStatement.setString(3, emailData[1]);//COMPANY_NAME
@@ -38,12 +69,12 @@ public class CsvEmailParser {
                 preparedStatement.setString(7, emailData[3]);//SIT
 
                 preparedStatement.executeUpdate();
-
-                connection.commit();
+                uniqueMails.add(emailData[0]);
 
                 System.out.println(emailData.length);
-                System.out.println("data " + emailData[0] + " " + emailData[1] + " " + emailData[5]);
+
             }
+            connection.commit();
         } catch (IOException e) {
             e.printStackTrace();
         } catch (SQLException e) {
